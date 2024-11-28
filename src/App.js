dbcdfb7cc600ac6040eff3dcc5ca412d1a778ef7
fatimahanif135 <fatimahanif332@gmail.com>
2024-11-28 19:27:37 +0500

    Initial commit with project setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css'
+import Stopwatch from './Component/Stopwatch';
+
+function App() {
+  return (
+    <div>
+  <Stopwatch/>
+  
+    </div>
+  );
+}
+
+export default App;
+ 
